72e36dfe860ef939fe466369646aa88961bbf2ec
dheerajkumar96 <dhirajkumar90001@gmail.com>
2021-03-05 18:17:01 +0530

    testcases changes

--- src/controllers/ticketController.js
+++ src/controllers/ticketController.js
@@ -5,7 +5,7 @@ const createTicket = async(req,res) => {
 let value = req.body;
 let query = `insert into tickets(creation,customer,performance,performancetime,ticket) values ('${value.creation}','${value.customer}','${value.performance}','${value.performancetime}',${value.ticket})`;
 database.database.query(query).then(data => {
-    res.send("created successfully");
+    res.status(200).send("created successfully");
 })
 .catch(err => {
     res.send(err);
@@ -18,10 +18,10 @@ const getTicket = async(req,res) => {
     let query = `select * from tickets where ticketid = ${value.ticketid}`;
     database.database.query(query, { type: database.database.QueryTypes.SELECT })
     .then(data => {
-        res.json(data);
+        res.status(200).json(data);
     })
     .catch(err => {
-        res.send(err);
+        res.status(500).send(err);
     })
 };
 
@@ -30,10 +30,10 @@ const deleteTicket = (req,res) => {
     let query = `update tickets set isDeleted=1 where ticketid = ${value.ticketid}`;
     database.database.query(query)
     .then(data => {
-        res.send('deleted successfully');
+        res.status(200).send('deleted successfully');
     })
     .catch(err => {
-        res.send(err);
+        res.status(500).send(err);
     })
 };
 
@@ -42,10 +42,10 @@ const updateTicket = (req,res) => {
     let query = `update tickets set performance = '${value.performance}' , performancetime ='${value.performancetime}' where ticketid=${value.ticketid}`;
     database.database.query(query)
     .then(data => {
-        res.send('updated successfully');
+        res.status(200).send('updated successfully');
     }) 
     .catch(err => {
-        res.json(err);
+        res.status(500).json(err);
     })
 };
 
@@ -59,17 +59,17 @@ const getAnalyticsByRevenue = async(req,res) => {
     await database.database.query(query)
     .then(data => {
         console.log(data);
-        res.json(data);
+        res.status(200).json(data);
     })
     .catch(err => {
-        res.send(err);
+        res.status(500).send(err);
     });
     }else if(value.method === 'js algorithms'){
         let query = `select creation date, sum(ticket) summaryProfit from tickets
         where creation between '${dates.startDate}' and '${dates.endDate}'
         group by creation`
         let data = await database.database.query(query);
-        res.json(data);
+        res.status(200).json(data);
     }
 
 };
@@ -84,17 +84,17 @@ const getAnalyticsByVisits = async(req,res) => {
     await database.database.query(query)
     .then(data => {
         console.log(data);
-        res.json(data);
+        res.status(200).json(data);
     })
     .catch(err => {
-        res.send(err);
+        res.status(500).send(err);
     });
     }else if(value.method === 'js algorithms'){
         let query = `select creation date, sum(ticket/150) summaryVisits from tickets
         where creation between '${dates.startDate}' and '${dates.endDate}'
         group by creation`
         let data = await database.database.query(query);
-        res.json(data);
+        res.status(200).json(data);
     }
 
 };
---
+++ src/test/ticket.test.js
@@ -0,0 +1,69 @@
+const expect = require('chai').expect,
+database = require('../database'),
+chaihttp = require('chai-http'),
+chai = require('chai'),
+app = require('../server'),
+assert = require('chai').assert,
+should = chai.should();
+
+chai.use(chaihttp);
+describe('ticket backend test', () => {
+    describe('get ticket method', function(){
+        this.timeout(10000);
+        it.only('should get ticket data when a valid ticketid is passed', async() => {
+            req = {
+                ticketid:2
+            };
+            const res = await chai.request('http://localhost:8085')
+            .get('/ticket/get')
+            .set('Content-Type', 'application/json')
+            .send(req);
+            assert.equal(res.status, 200);
+        });
+        });
+    describe('create ticket method', function(){
+        this.timeout(10000);
+        it.only('should create ticket when a valid request body is passed', async() => {
+            req = {
+                creation: "05-04-2021 09:00:00",
+            customer: "dheeraj5",
+            performance :"dheerajmovie5",
+            performancetime: "05-04-2021 12:00:00",
+            ticket:150
+            };
+            const res = await chai.request('http://localhost:8085')
+            .post('/ticket/create')
+            .set('Content-Type', 'application/json')
+            .send(req);
+            assert.equal(res.status, 200);
+        });
+        });
+    describe('update ticket method', function(){
+        this.timeout(10000);
+        it.only('should update ticket data when a valid ticketid and data is passed', async() => {
+            req = {
+                performance:"movie4",
+                performancetime:"04-03-2021 02:00:00",
+                ticketid:2
+            }
+            const res = await chai.request('http://localhost:8085')
+            .put('/ticket/update')
+            .set('Content-Type', 'application/json')
+            .send(req);
+            assert.equal(res.status, 200);
+        });
+        });
+    describe('delete ticket method', function(){
+        this.timeout(10000);
+        it.only('should delete ticket data when a valid ticketid is passed', async() => {
+            req = {
+                ticketid:3
+            };
+            const res = await chai.request('http://localhost:8085')
+            .delete('/ticket/delete')
+            .set('Content-Type', 'application/json')
+            .send(req);
+            assert.equal(res.status, 200);
+        });
+        });
+});
